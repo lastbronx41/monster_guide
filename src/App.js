606119cc925dffdaa34243a6911f1b2617da0398
lastbronx41 <lastbronx41@gmail.com>
2022-12-08 01:38:53 +0000

    creating filtering functionality

--- src/App.js
+++ src/App.js
@@ -4,23 +4,42 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        ))
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => { console.log(event.target.value);
+        const filteredMonsters = this.state.monsters.filter((monster) => {return monster.name.includes(event.target.value)}) }} />
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          )
+        })
+        }
+
+
       </div>
     );
   }
